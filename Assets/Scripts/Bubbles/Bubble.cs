869fbc15b4bdc5478bc1ebe551d0c0f2dd402699
Konstantin Kuznetsov <stalkercatkuz@gmail.com>
2025-01-19 00:47:29 +0700

    add bubble push

--- Assets/Scripts/Bubbles/Bubble.cs
+++ Assets/Scripts/Bubbles/Bubble.cs
@@ -1,10 +1,17 @@
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Bubbles
 {
     public class Bubble : MonoBehaviour
     {
+        [SerializeField]
+        private float pushDistance;
+
+        [SerializeField]
+        private float pushSpeed;
+
         [SerializeField]
         private SpriteRenderer spriteRenderer;
 
@@ -14,14 +21,18 @@ namespace Bubbles
         [SerializeField]
         private float followSpeed;
 
-        private TrailRenderer target;
-        private int index;
+        private TrailRenderer _target;
+        private int _index;
+        private Vector3 initPosition;
+        private Vector3 pushDirection;
+        private Vector3 targetPosition;
 
-        public int Index => index;
+        public int Index => _index;
         public bool IsConnected { get; private set; }
 
         private void Start()
         {
+            initPosition = transform.position;
             if (sprites.Count > 0)
             {
                 spriteRenderer.sprite = sprites[Random.Range(0, sprites.Count - 1)];
@@ -31,26 +42,59 @@ namespace Bubbles
         public void Connect(TrailRenderer trailRenderer, int followIndex)
         {
             IsConnected = true;
-            target = trailRenderer;
-            index = followIndex;
+            _target = trailRenderer;
+            _index = followIndex;
         }
 
         public void Consume()
         {
             gameObject.SetActive(false);
             IsConnected = false;
-            target = null;
-            index = -1;
+            _target = null;
+            _index = -1;
         }
 
         public void Update()
         {
-            if (IsConnected && target.positionCount > 0)
+            if (IsConnected && _target.positionCount > 0)
             {
-                int positionCount = target.positionCount;
-                Vector3 nextPosition = target.GetPosition(Mathf.Clamp(positionCount - index - 1, 0, positionCount));
+                int positionCount = _target.positionCount;
+                Vector3 nextPosition = _target.GetPosition(Mathf.Clamp(positionCount - _index - 1, 0, positionCount));
                 transform.position = Vector3.Lerp(transform.position, nextPosition, Time.deltaTime * followSpeed);
             }
+
+            if (IsConnected || pushDistance < Mathf.Epsilon)
+            {
+                return;
+            }
+
+            if (pushDirection != Vector3.zero)
+            {
+                targetPosition = initPosition + pushDirection.normalized * pushDistance;
+                pushDirection = Vector3.zero;
+            }
+
+            bool targetReached = Vector3.Distance(transform.position, targetPosition) < 0.1f;
+
+            if (targetReached)
+            {
+                targetPosition = initPosition;
+            }
+
+            if (!targetReached)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetPosition, pushSpeed * Time.deltaTime);
+            }
+        }
+
+        public void Push(Vector3 direction)
+        {
+            if (IsConnected)
+            {
+                return;
+            }
+
+            pushDirection = direction.normalized;
         }
     }
 }
