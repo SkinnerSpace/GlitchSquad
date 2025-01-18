02424a87dce382c5071b5ffbe87f36f2aecce1ca
Skinner Space <70509103+SkinnerSpace@users.noreply.github.com>
2025-01-19 04:56:47 +0700

    Fix next scene bug

--- Assets/Scripts/SceneManage.cs
+++ Assets/Scripts/SceneManage.cs
@@ -12,10 +12,12 @@ public class SceneManage : MonoBehaviour
     public void LoadNextScene()
     {
         int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+
         if (nextSceneIndex > SceneManager.sceneCount)
         {
-            nextSceneIndex = 1;
+            nextSceneIndex = 2;
         }
+
         SceneManager.LoadScene(nextSceneIndex);
         Time.timeScale = 1f;
     }
